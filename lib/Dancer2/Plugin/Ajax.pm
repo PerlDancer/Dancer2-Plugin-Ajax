334eaa2dc20b4bf653b897610220fed4596d71c8
Sawyer X <xsawyerx@cpan.org>
2014-11-26 23:41:23 +0100

    Use is_ajax from Core::Request, cleanup:
    I wish we could remove is_ajax right away, but we can't. It's
    currently in Dancer2::Core::Request (and should be deprecated and
    moved in here) but for now we'll use it until it's deprecated.

--- lib/Dancer2/Plugin/Ajax.pm
+++ lib/Dancer2/Plugin/Ajax.pm
@@ -1,24 +1,53 @@
-# ABSTRACT: a plugin for adding Ajax route handlers
-
 package Dancer2::Plugin::Ajax;
+# ABSTRACT: a plugin for adding Ajax route handlers
 
 use strict;
 use warnings;
-
-use Dancer2;
 use Dancer2::Plugin;
 
-=head1 NAME
+register 'ajax' => sub {
+    my ( $dsl, $pattern, @rest ) = @_;
 
-Dancer2::Plugin::Ajax - A plugin for adding Ajax route handlers
+    my $default_methods = [ 'get', 'post' ];
 
-=head1 VERSION
+    # If the given pattern is an ArrayRef, we override the defaults
+    # and pass these onto to DSL->any()
+    if( ref($pattern) eq "ARRAY" ) {
+        $default_methods = $pattern;
+        $pattern = shift(@rest);
+    }
 
-Version 0.01
+    my $code;
+    for my $e (@rest) { $code = $e if ( ref($e) eq 'CODE' ) }
 
-=cut
+    my $content_type = plugin_setting->{content_type} || 'text/xml';
 
-our $VERSION = '0.01';
+    my $ajax_route = sub {
+
+        # # must be an XMLHttpRequest
+        if ( not $dsl->request->is_ajax ) {
+            $dsl->pass and return 0;
+        }
+
+        # Default response content type is either what's defined in the
+        # plugin setting or text/xml
+        $dsl->response->header('Content-Type')
+          or $dsl->response->content_type( $content_type );
+
+        # disable layout
+        my $layout = $dsl->setting('layout');
+        $dsl->setting( 'layout' => undef );
+        my $response = $code->();
+        $dsl->setting( 'layout' => $layout );
+        return $response;
+    };
+
+    $dsl->any( $default_methods => $pattern, $ajax_route );
+};
+
+register_plugin;
+
+1;
 
 =head1 SYNOPSIS
 
@@ -73,74 +102,3 @@ Here is example to use JSON:
     Ajax:
       content_type: 'application/json'
 
-=cut
-
-sub is_ajax {
-    my ($request) = @_;
-
-    if (defined $request->header('x-requested-with')
-            and $request->header('x-requested-with') eq 'XMLHttpRequest') {
-        return 1;
-    }
-    return 0;
-}
-
-register 'ajax' => sub {
-    my ( $dsl, $pattern, @rest ) = @_;
-
-    my $default_methods = [ 'get', 'post' ];
-
-    # If the given pattern is an ArrayRef, we override the defaults
-    # and pass these onto to DSL->any()
-    if( ref($pattern) eq "ARRAY" ) {
-        $default_methods = $pattern;
-        $pattern = shift(@rest);
-    }
-
-    my $code;
-    for my $e (@rest) { $code = $e if ( ref($e) eq 'CODE' ) }
-
-    my $content_type = plugin_setting->{content_type} || 'text/xml';
-
-    my $ajax_route = sub {
-
-        # # must be an XMLHttpRequest
-        if ( not is_ajax($dsl->request) ) {
-            $dsl->pass and return 0;
-        }
-
-        # Default response content type is either what's defined in the
-        # plugin setting or text/xml
-        $dsl->response->header('Content-Type')
-          or $dsl->response->content_type( $content_type );
-
-        # disable layout
-        my $layout = $dsl->setting('layout');
-        $dsl->setting( 'layout' => undef );
-        my $response = $code->();
-        $dsl->setting( 'layout' => $layout );
-        return $response;
-    };
-
-    $dsl->any( $default_methods => $pattern, $ajax_route );
-};
-
-=head1 AUTHOR
-
-Alexis Sukrieh, C<< <sukria at sukria.net> >>
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2014 Alexis Sukrieh.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See L<http://dev.perl.org/licenses/> for more information.
-
-=cut
-
-register_plugin;
-
-1;
